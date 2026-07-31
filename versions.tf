9b5f8c273c3af565b3936833b8bc4a2c1321eb63
Cheppali <mastan.cheppali-ext@hpe.com>
2026-08-01 00:40:33 +0530

    added

--- versions.tf
+++ versions.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = "~> 1.5.0"
 
-  backend "s3" {
-    bucket  = "shopverse-ecom-terraform-state"
-    key     = "eks/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
